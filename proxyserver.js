8f455dadaa359100ab61a483c9cae06e47872f99
studystyle <info.studystyle@gmail.com>
2022-05-11 13:55:38 +1200

    Added API to quiz

---
+++ proxyserver.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/api/getStudyStyleQuestion', (req, res) => {
+  request(
+    { url: 'https://z0rfo1.deta.dev/api/getStudyStyleQuestion' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
--- quiz/index.js
+++ quiz/index.js
@@ -1,8 +1,18 @@
+// install express with `npm install express` 
+const express = require('express')
+const app = express()
+
+
+// export 'app'
+module.exports = app
+
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -11,17 +21,38 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('https://z0rfo1.deta.dev/api/getStudyStyleQuestion')
+fetch(
+    'https://z0rfo1.deta.dev/api/getStudyStyleQuestion'
+)
     .then((res) => {
-        console.log(res);
         return res.json();
     })
-    .then(loadedQuestions => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
         startGame();
+    })
+    .catch((err) => {
+        console.error(err);
     });
-    
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -30,8 +61,10 @@ const MAX_QUESTIONS = 3;
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuesions = [questions];
+    availableQuesions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
