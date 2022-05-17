1237907975260feea73f7148e20cec6f9b14af91
studystyle <info.studystyle@gmail.com>
2022-05-17 18:42:38 +1200

    Get from questions.json file

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-/* const progressText = document.getElementById('progressText');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull'); */
+const progressBarFull = document.getElementById('progressBarFull');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -11,19 +11,12 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch(
-    'https://z0rfo1.deta.dev/api/getStudyStyleQuestion'
-)
+fetch('questions.json')
     .then((res) => {
         return res.json();
     })
-    //Pulls question and choices from API
     .then((loadedQuestions) => {
-        console.log(loadedQuestions);
-       
-        questions = loadedQuestions
-
-
+        questions = loadedQuestions;
         startGame();
     })
     .catch((err) => {
@@ -37,128 +30,66 @@ const MAX_QUESTIONS = 3;
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuesions = [questions];
+    availableQuesions = [...questions];
     getNewQuestion();
 };
 
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        //go to the end page
-        return window.location.assign('/end.html');
+      //go to the end page
+      return window.location.assign("/end.html");
     }
-
-    //Assigns question with number between 1-4 
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    //Links choice to html
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    //Makes sure that question does not get repeated
-    availableQuesions.splice(questionIndex, 1);
-    acceptingAnswers = true;
-};
-
-//When choice is click, supposed to go to new question. But it doesn't
-
-choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-        getNewQuestion();
-    });
-});
-
-/* //CONSTANTS
-const VISUAL = 10;
-const AUDITORY = 5;
-const MAX_QUESTIONS = 3;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [questions];
-    getNewQuestion();
-};
-
-
-getNewQuestion = () => {
-    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('/end.html');
-    }
-
-    //Counts how many questions have been answered
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
+  
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
+  
 
-    //Places choice into boxes in html
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+
+    choices.forEach(choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion["choice" + number];
     });
-
+  
     availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
-};
+  };
 
 choices.forEach((choice) => {
     choice.addEventListener('click', (e) => {
         if (!acceptingAnswers) return;
+       
+        
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
+        console.log(selectedAnswer == currentQuestion.answer)
 
-        const classToApply =
-            selectedAnswer == currentQuestion.answer ? 'v' : 'a';
 
-        if (classToApply === 'v') {
-            incrementScore(VISUAL);
-        }
+        console.log(availableQuesions)
+        const classToApply = 'incorrect';
+            if (selectedAnswer === currentQuestion.answer) {
+                classToApply = 'correct';
+            }
 
-        if (classToApply === 'a') {
-            incrementScore(AUDITORY);
-        }
+            selectedChoice.parentElement.classList.add(classToApply);
 
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
+            setTimeout(() => {
+                selectedChoice.parentElement.classList.remove(classToApply);
+                getNewQuestion();
+            }, 1000);
     });
+ 
 });
+
 
 incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
 };
- */
 
-
-
-/* .then((res) => {
-    console.log(res);
-    return res.json();
-})
-.then(loadedQuestions => {
-    console.log(loadedQuestions);
-    questions = loadedQuestions;
-    startGame();
-}); */
