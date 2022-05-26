d2d20517a292811a84d11f45b6e49c6970f21eaf
studystyle <info.studystyle@gmail.com>
2022-05-26 21:42:50 +1200

    Added styling and changed file names

--- quiz.js
+++ quiz.js
@@ -24,7 +24,7 @@ fetch('questions.json')
     });
 
 //CONSTANT
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 8;
 
 startGame = () => {
     questionCounter = 0;
@@ -36,7 +36,7 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
       //go to the end page
-      return window.location.assign("./end.html");
+      return window.location.assign("./endquiz.html");
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
